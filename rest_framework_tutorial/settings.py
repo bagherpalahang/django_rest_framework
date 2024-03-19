0a04a01a4441977af0cdb8aa9088a5591299c588
bagher <palahangmohammadbagher@gmail.com>
2024-03-19 05:59:49 +0330

    blog app

--- rest_framework_tutorial/settings.py
+++ rest_framework_tutorial/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # apps
+    'blog',
+
+    # 3rd app party
+    'rest_framework',
+
 ]
 
 MIDDLEWARE = [
@@ -54,7 +60,7 @@ ROOT_URLCONF = 'rest_framework_tutorial.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
