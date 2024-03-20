550a096795112d5e569bf70200adb1fa0dfce4e1
bagher <palahangmohammadbagher@gmail.com>
2024-03-20 17:26:01 +0330

    some custom permissions

--- api/views.py
+++ api/views.py
@@ -1,10 +1,11 @@
 # from django.shortcuts import render
 from django.contrib.auth.models import User
-from rest_framework.permissions import IsAdminUser
+# from rest_framework.permissions import IsAdminUser
 from rest_framework.generics import ListAPIView, ListCreateAPIView, RetrieveAPIView, RetrieveUpdateDestroyAPIView
 
 from blog.models import Article
 from .serializers import ArticleSerializer, UserSerializer
+from .permissions import IsSuperUser, IsAuthorOrReadOnly, IsStaffOrReadOnly, IsSuperUserOrStaffReadOnly, IsOwnerOrStaffReadOnly
 
 # Create your views here.
 
@@ -12,19 +13,21 @@ class ArticleList(ListCreateAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
 
+
 class ArticleDetail(RetrieveUpdateDestroyAPIView):
+    parser_classes = [IsStaffOrReadOnly, IsAuthorOrReadOnly]
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
     lookup_field = 'slug'
 
-class UserList(ListCreateAPIView):
-    permission_classes = [IsAdminUser]
 
+class UserList(ListCreateAPIView):
+    permission_classes = [IsSuperUserOrStaffReadOnly]
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
-class UserDetail(RetrieveUpdateDestroyAPIView):
-    permission_classes = [IsAdminUser]
 
+class UserDetail(RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsOwnerOrStaffReadOnly]
     queryset = User.objects.all()
     serializer_class = UserSerializer
